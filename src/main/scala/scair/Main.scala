827da431829cf458cbbc3d8f68e28a49aa6b22a1
Emilien Bauer <bauer.emilien@gmail.com>
2024-06-11 20:39:42 +0100

    Add scalafmt as an sbt plugin, add CI checks.

--- src/main/scala/scair/Main.scala
+++ src/main/scala/scair/Main.scala
@@ -3,53 +3,56 @@ package scair
 class Region // TODO
 
 case class Block(
-	operations: Seq[Operation],
-	argumentTypes: Seq[Attribute]
+    operations: Seq[Operation],
+    argumentTypes: Seq[Attribute]
 ) {
 
-	// Initialization Block
-	{
-		
-	}
+  // Initialization Block
+  {}
 
-	override def toString(): String = {
-		return "behold, i am block"
-	}
+  override def toString(): String = {
+    return "behold, i am block"
+  }
 }
 
 case class Attribute(
-	name: String
+    name: String
 ) {
-	override def toString(): String = {
-		return s"$name"
-	}
+  override def toString(): String = {
+    return s"$name"
+  }
 }
-	
-case class Value(
-	//val name: String,
-	typ: Attribute	
-){
-	override def toString(): String = {
-		return s"%sv"
-	}
-}
-	
-case class Operation(
-	name: String,
-	operands: Seq[Value], // TODO rest
-	results: Seq[Value]
-){
-	override def toString(): String = {
-		val resultsStr: String = if (results.length > 0) results.mkString(", ") + " = " else ""
-		val operandsStr: String = if (operands.length > 0) operands.mkString(", ") else ""
-		val functionType: String = "(" + operands.map(x => x.typ).mkString(", ") + ") -> (" + results.map(x => x.typ).mkString(", ") + ")"
 
-		return resultsStr + "\"" + name + "\" (" + operandsStr +") : " + functionType 
-	}
+case class Value(
+    // val name: String,
+    typ: Attribute
+) {
+  override def toString(): String = {
+    return s"%sv"
+  }
 }
-	
+
+case class Operation(
+    name: String,
+    operands: Seq[Value], // TODO rest
+    results: Seq[Value]
+) {
+  override def toString(): String = {
+    val resultsStr: String =
+      if (results.length > 0) results.mkString(", ") + " = " else ""
+    val operandsStr: String =
+      if (operands.length > 0) operands.mkString(", ") else ""
+    val functionType: String =
+      "(" + operands.map(x => x.typ).mkString(", ") + ") -> (" + results
+        .map(x => x.typ)
+        .mkString(", ") + ")"
+
+    return resultsStr + "\"" + name + "\" (" + operandsStr + ") : " + functionType
+  }
+}
+
 object Main {
-	def main(args: Array[String]): Unit = {
-		println("TODO compiler")
-	}
+  def main(args: Array[String]): Unit = {
+    println("TODO compiler")
+  }
 }
