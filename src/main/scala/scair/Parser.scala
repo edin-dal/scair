07c5bd16ccc2ebfddff8a78d737bdf2eed9c9fbf
Maks Kret <s2071662@ed.ac.uk>
2024-06-14 13:30:50 +0100

    Parsing Regions and Regions within Operations (#4)

--- src/main/scala/scair/Parser.scala
+++ src/main/scala/scair/Parser.scala
@@ -5,40 +5,106 @@ import scala.collection.mutable
 import scala.util.{Try, Success, Failure}
 import Main._
 
-object Parser {
+class Parser(
+    var valueMap: mutable.Map[String, Value] = mutable.Map.empty[String, Value]
+) {
 
   //////////////////////
   // COMMON FUNCTIONS //
   //////////////////////
 
-  var valueMap: mutable.Map[String, Value] = mutable.Map.empty[String, Value]
+  // Custom function wrapper that allows to Escape out of a pattern
+  // to carry out custom computation
+  def E[_: P](action: => Unit) = {
+    action
+    Pass()
+  }
 
-  // context is kept in a class, and each one automatically swaps out the valueMap based on its local value map
-  class Context(var localValueMap: mutable.Map[String, Value]) {
+  object Scope {
 
-    // we first slot in the local context into the Parser object, then slot it back out once parsing is done
-    def parseThis(text: String): fastparse.Parsed[Seq[Operation]] = {
-      valueMap = localValueMap
-      val result = parse(text, TopLevel(_))
-      localValueMap = valueMap
-      return result
+    def defineValues(
+        valueIdAndTypeList: Seq[(String, Attribute)]
+    )(implicit
+        scope: Scope
+    ): Seq[Value] = {
+      for {
+        (name, typ) <- valueIdAndTypeList
+      } yield scope.valueMap.contains(name) match {
+        case true =>
+          throw new Exception(s"SSA Value cannot be defined twice %${name}")
+        case false =>
+          val value = new Value(typ = typ)
+          scope.valueMap(name) = value
+          value
+      }
     }
 
-    // child starts off from the parents context
-    def createChild(): Context = {
-      return new Context(localValueMap = localValueMap)
+    def useValues(
+        valueIdAndTypeList: Seq[(String, Attribute)]
+    )(implicit
+        scope: Scope
+    ): Seq[Value] = {
+      for {
+        (name, typ) <- valueIdAndTypeList
+      } yield !scope.valueMap.contains(name) match {
+        case true =>
+          throw new Exception(s"SSA value used but not defined %${name}")
+        case false =>
+          scope.valueMap(name).typ != typ match {
+            case true =>
+              throw new Exception(
+                s"$name use with type ${typ} but defined with type ${scope.valueMap(name).typ}"
+              )
+            case false =>
+              scope.valueMap(name)
+          }
+      }
     }
 
-    def getLocalValueMap(): mutable.Map[String, Value] = localValueMap
-
-    def testParse[A](
-        text: String,
-        parser: fastparse.P[_] => fastparse.P[A]
-    ): fastparse.Parsed[A] = {
-      valueMap = mutable.Map.empty[String, Value]
-      return parse(text, parser)
+    def defineBlock(
+        blockName: String,
+        block: Block
+    )(implicit
+        scope: Scope
+    ): Unit = scope.blockMap.contains(blockName) match {
+      case true =>
+        throw new Exception(
+          s"Block cannot be defined twice within the same scope - ^${blockName}"
+        )
+      case false =>
+        scope.blockMap(blockName) = block
     }
   }
+
+  class Scope(
+      var valueMap: mutable.Map[String, Value],
+      var blockMap: mutable.Map[String, Block] =
+        mutable.Map.empty[String, Block],
+      var parentScope: Option[Scope] = None
+  ) {
+
+    // child starts off from the parents context
+    def createChild(): Scope = {
+      return new Scope(
+        valueMap = valueMap,
+        blockMap = blockMap,
+        parentScope = Some(this)
+      )
+    }
+
+    def switchWithChild(): Unit = {
+      currentScope = createChild()
+    }
+
+    def switchWithParent(): Unit = parentScope match {
+      case Some(x) =>
+        currentScope = x
+      case None =>
+        throw new Exception("No parent present - check your")
+    }
+  }
+
+  implicit var currentScope: Scope = new Scope(valueMap = valueMap)
 
   ///////////////////
   // COMMON SYNTAX //
@@ -55,26 +121,35 @@ object Parser {
   // [x] float-literal ::= [-+]?[0-9]+[.][0-9]*([eE][-+]?[0-9]+)?
   // [x] string-literal  ::= `"` [^"\n\f\v\r]* `"`
 
+  val excludedCharacters: Set[Char] = Set('\"', '\n', '\f', '\u000B',
+    '\r') // \u000B represents \v escape character
+
   def Digit[$: P] = P(CharIn("0-9").!)
+
   def HexDigit[$: P] = P(CharIn("0-9a-fA-F").!)
+
   def Letter[$: P] = P(CharIn("a-zA-Z").!)
+
   def IdPunct[$: P] = P(CharIn("$._\\-").!)
 
   def IntegerLiteral[$: P] = P(HexadecimalLiteral | DecimalLiteral)
+
   def DecimalLiteral[$: P] =
     P(Digit.rep(1).!).map((literal: String) => literal.toInt)
+
   def HexadecimalLiteral[$: P] =
     P("0x" ~~ HexDigit.rep(1).!).map((hex: String) => Integer.parseInt(hex, 16))
+
   def FloatLiteral[$: P] = P(
     CharIn("\\-\\+").? ~~ DecimalLiteral ~~ "." ~~ DecimalLiteral ~~ (CharIn(
       "eE"
     ) ~~ CharIn("\\-\\+").? ~~ DecimalLiteral).?
   ).! // substituted [0-9]* with [0-9]+
-  val excludedCharacters: Set[Char] = Set('\"', '\n', '\f', '\u000B',
-    '\r') // \u000B represents \v escape character
+
   def notExcluded[$: P] = P(
     CharPred(char => !excludedCharacters.contains(char))
   )
+
   def StringLiteral[$: P] = P("\"" ~~ notExcluded.rep.! ~~ "\"")
 
   //////////////////////////
@@ -111,14 +186,18 @@ object Parser {
     }
 
   def BareId[$: P] = P((Letter | "_") ~~ (Letter | Digit | CharIn("_$.")).rep).!
+
   def ValueId[$: P] = P("%" ~~ SuffixId)
+
   def AliasName[$: P] = P(BareId)
+
   def SuffixId[$: P] = P(
     DecimalLiteral | (Letter | IdPunct) ~~ (Letter | IdPunct | Digit).rep
   ).!
 
   def ValueUse[$: P] =
     P(ValueId ~ ("#" ~ DecimalLiteral).?).map(simplifyValueName)
+
   def ValueUseList[$: P] = P(ValueUse ~ ("," ~ ValueUse).rep).map(
     (valueUseList: (String, Seq[String])) => valueUseList._1 +: valueUseList._2
   )
@@ -138,29 +217,29 @@ object Parser {
   // [ ] successor-list        ::= `[` successor (`,` successor)* `]`
   // [ ] successor             ::= caret-id (`:` block-arg-list)?
   // [ ] dictionary-properties ::= `<` dictionary-attribute `>`
-  // [ ] region-list           ::= `(` region (`,` region)* `)`
+  // [x] region-list           ::= `(` region (`,` region)* `)`
   // [ ] dictionary-attribute  ::= `{` (attribute-entry (`,` attribute-entry)*)? `}`
   // [x] trailing-location     ::= `loc` `(` location `)`
 
-  //                                     results           name        operands            op types       res types
+  //  results      name     operands     op types      res types
   def generateOperation(
       operation: (
-          Option[Seq[String]],
-          (String, Option[Seq[String]], (Seq[Attribute], Seq[Attribute]))
+          Seq[String],
+          (
+              String,
+              Seq[String],
+              Seq[Region],
+              (Seq[Attribute], Seq[Attribute])
+          )
       )
   ): Operation = {
 
-    val results: Seq[String] = operation._1 match {
-      case None    => Seq[String]()
-      case Some(x) => x
-    }
+    val results: Seq[String] = operation._1
     val opName = operation._2._1
-    val operands: Seq[String] = operation._2._2 match {
-      case None    => Seq[String]()
-      case Some(x) => x
-    }
-    val resultsTypes = operation._2._3._2
-    val operandsTypes = operation._2._3._1
+    val operands: Seq[String] = operation._2._2
+    val regions: Seq[Region] = operation._2._3
+    val resultsTypes = operation._2._4._2
+    val operandsTypes = operation._2._4._1
 
     if (results.length != resultsTypes.length) {
       throw new Exception("E")
@@ -170,38 +249,15 @@ object Parser {
       throw new Exception("E")
     }
 
-    val resultss: Seq[Value] = for {
-      n <- (0 to results.length - 1)
-    } yield valueMap.contains(results(n)) match {
-      case true =>
-        throw new Exception(s"SSA Value cannot be defined twice %${results(n)}")
-      case false =>
-        val value = new Value(typ = resultsTypes(n))
-        valueMap(results(n)) = value
-        value
-    }
+    val resultss: Seq[Value] = Scope.defineValues(results zip resultsTypes)
 
-    val operandss: Seq[Value] = for {
-      n <- { 0 to operands.length - 1 }
-    } yield !valueMap.contains(operands(n)) match {
-      case true =>
-        throw new Exception(s"SSA value used but not defined %${operands(n)}")
-      case false =>
-        val name = operands(n)
-        valueMap(name).typ != operandsTypes(n) match {
-          case true =>
-            throw new Exception(
-              s"$name use with type ${operandsTypes(n)} but defined with type ${valueMap(name).typ}"
-            )
-          case false =>
-            valueMap(name)
-        }
-    }
+    val operandss: Seq[Value] = Scope.useValues(operands zip operandsTypes)
 
     return new Operation(
       name = opName,
       operands = operandss,
-      results = resultss
+      results = resultss,
+      regions = regions
     )
   }
 
@@ -210,17 +266,30 @@ object Parser {
     case (name, None)          => Seq(name)
   }
 
-  def OperationPat[$: P] = P(
-    OpResultList.? ~ GenericOperation ~ TrailingLocation.?
+  def optionlessThis[A](option: Option[Seq[A]]): Seq[A] = option match {
+    case None    => Seq[A]()
+    case Some(x) => x
+  }
+
+  def OperationPat[$: P]: P[Operation] = P(
+    OpResultList.?.map(optionlessThis) ~ GenericOperation ~ TrailingLocation.?
   ).map(generateOperation) // shortened definition TODO: finish...
+
   def GenericOperation[$: P] = P(
-    StringLiteral ~ "(" ~ ValueUseList.? ~ ")" ~ ":" ~ FunctionType
+    StringLiteral ~ "(" ~ ValueUseList.?.map(
+      optionlessThis
+    ) ~ ")" ~ RegionList.?.map(optionlessThis) ~ ":" ~ FunctionType
   ) // shortened definition TODO: finish...
+
   def OpResultList[$: P] = P(OpResult ~ ("," ~ OpResult).rep ~ "=").map(
     (results: (Seq[String], Seq[Seq[String]])) =>
       results._1 ++ results._2.flatten
   )
   def OpResult[$: P] = P(ValueId ~ (":" ~ IntegerLiteral).?).map(sequenceValues)
+
+  def RegionList[$: P] = P("(" ~ Region ~ ("," ~ Region).rep ~ ")")
+    .map((x: (Region, Seq[Region])) => x._1 +: x._2)
+
   def TrailingLocation[$: P] = P(
     "loc" ~ "(" ~ "unknown" ~ ")"
   ) // definition taken from xdsl attribute_parser.py v-0.19.0 line 1106
@@ -229,23 +298,83 @@ object Parser {
   // BLOCKS //
   ////////////
 
-  // [ ] - block           ::= block-label operation+
-  // [ ] - block-label     ::= block-id block-arg-list? `:`
-  // [ ] - block-id        ::= caret-id
-  // [ ] - caret-id        ::= `^` suffix-id
-  // [ ] - value-id-and-type ::= value-id `:` type
+  // [x] - block           ::= block-label operation+
+  // [x] - block-label     ::= block-id block-arg-list? `:`
+  // [x] - block-id        ::= caret-id
+  // [x] - caret-id        ::= `^` suffix-id
+  // [x] - value-id-and-type ::= value-id `:` type
 
   // // Non-empty list of names and types.
-  // [ ] - value-id-and-type-list ::= value-id-and-type (`,` value-id-and-type)*
+  // [x] - value-id-and-type-list ::= value-id-and-type (`,` value-id-and-type)*
 
-  // [ ] - block-arg-list ::= `(` value-id-and-type-list? `)`
+  // [x] - block-arg-list ::= `(` value-id-and-type-list? `)`
+
+  def createBlock(
+      //            name    argument     operations
+      uncutBlock: (String, Seq[Value], Seq[Operation])
+  ): Block = {
+    val newBlock = new Block(
+      operations = uncutBlock._3,
+      arguments = uncutBlock._2
+    )
+
+    Scope.defineBlock(uncutBlock._1, newBlock)
+    return newBlock
+  }
+
+  def elimOption(
+      valueIdAndTypeList: Option[Seq[(String, Attribute)]]
+  ): Seq[(String, Attribute)] = valueIdAndTypeList match {
+    case Some(x: Seq[(String, Attribute)]) => x
+    case None                              => Seq()
+  }
+
+  def Block[$: P] = P(BlockLabel ~ OperationPat.rep(1)).map(createBlock)
+
+  def BlockLabel[$: P] = P(
+    BlockId ~ BlockArgList.?.map(elimOption)
+      .map(Scope.defineValues) ~ ":"
+  )
+
+  def BlockId[$: P] = P(CaretId)
+
+  def CaretId[$: P] = P("^" ~ SuffixId)
+
+  def ValueIdAndType[$: P] = P(ValueId ~ ":" ~ Type)
+
+  def ValueIdAndTypeList[$: P] =
+    P(ValueIdAndType ~ ("," ~ ValueIdAndType).rep).map(
+      (idAndTypes: (String, Attribute, Seq[(String, Attribute)])) =>
+        (idAndTypes._1, idAndTypes._2) +: idAndTypes._3
+    )
+
+  def BlockArgList[$: P] = P("(" ~ ValueIdAndTypeList.? ~ ")").map(elimOption)
 
   /////////////
   // REGIONS //
   /////////////
 
-  // [ ] - region        ::= `{` entry-block? block* `}`
-  // [ ] - entry-block   ::= operation+
+  // [x] - region        ::= `{` entry-block? block* `}`
+  // [x] - entry-block   ::= operation+
+  //                    |
+  //                    |  rewritten as
+  //                   \/
+  // [x] - region        ::= `{` operation* block* `}`
+
+  def defineRegion(parseResult: (Seq[Operation], Seq[Block])): Region = {
+    return new Region(blocks = parseResult._2)
+  }
+
+  // EntryBlock might break - take out if it does...
+  def Region[$: P] = P(
+    "{" ~ E(
+      currentScope.switchWithChild
+    ) ~ OperationPat.rep ~ Block.rep ~ "}" ~ E(
+      currentScope.switchWithParent
+    )
+  ).map(defineRegion)
+
+  // def EntryBlock[$: P] = P(OperationPat.rep(1))
 
   ///////////
   // TYPES //
@@ -274,21 +403,26 @@ object Parser {
   def BuiltIn[$: P] = P(I32 | I64) // temporary BuiltIn
 
   def Type[$: P] = P(TypeAlias | BuiltIn).map((typeName: String) =>
-    Seq(new Attribute(name = typeName))
+    new Attribute(name = typeName)
   ) // shortened definition TODO: finish...
 
-  def TypeListNoParens[$: P] = P(Type ~ ("," ~ Type).rep).map(
-    (types: (Seq[Attribute], Seq[Seq[Attribute]])) =>
-      types._1 ++ types._2.flatten
-  )
+  def TypeListNoParens[$: P] =
+    P(Type ~ ("," ~ Type).rep).map((types: (Attribute, Seq[Attribute])) =>
+      types._1 +: types._2
+    )
+
   def TypeListParens[$: P] = P(ClosedParens | "(" ~ TypeListNoParens ~ ")")
+
   def ClosedParens[$: P] = P("(" ~ ")").map(_ => Seq[Attribute]())
 
   def FunctionType[$: P] = P(
-    (Type | TypeListParens) ~ "->" ~ (Type | TypeListParens)
+    (Type.map(Seq(_)) | TypeListParens) ~ "->" ~ (Type.map(
+      Seq(_)
+    ) | TypeListParens)
   )
 
   def TypeAliasDef[$: P] = P("!" ~~ AliasName ~ "=" ~ Type)
+
   def TypeAlias[$: P] = P("!" ~~ AliasName)
 
   ////////////////
@@ -338,27 +472,78 @@ object Parser {
   //                             | `{` dialect-attribute-contents+ `}`
   //                             | [^\[<({\]>)}\0]+
 
-  //////////////////////////////////
-  // FUNCTIONAL TESTING ON THE GO //
-  //////////////////////////////////
+  def parseThis(text: String): fastparse.Parsed[Seq[Operation]] = {
+    return parse(text, TopLevel(_))
+  }
+
+  def testParse[A](
+      text: String,
+      parser: fastparse.P[_] => fastparse.P[A]
+  ): fastparse.Parsed[A] = {
+    currentScope = new Scope(valueMap = mutable.Map.empty[String, Value])
+    return parse(text, parser)
+  }
+}
+
+object Parser {
 
   def main(args: Array[String]): Unit = {
 
     println("---- IN PROGRESS ----")
 
-    val context1 = new Context(localValueMap = mutable.Map.empty[String, Value])
+    val parser = new Parser()
 
     println(
-      context1.testParse(
+      parser.testParse(
         text = "%0, %1, %2 = \"test.op\"() : () -> (i32, i64, i32)",
-        parser = TopLevel(_)
+        parser = parser.TopLevel(_)
       )
     )
     println(
-      context1.testParse(
+      parser.testParse(
         text = "%0, %1, %2 = \"test.op\"() : () -> (i32, i64, i32)\n" +
           "\"test.op\"(%1, %0) : (i64, i32) -> ()",
-        parser = TopLevel(_)
+        parser = parser.TopLevel(_)
+      )
+    )
+
+    println(
+      parser.testParse(
+        text =
+          "^bb0(%5: i32):\n" + "%0, %1, %2 = \"test.op\"() : () -> (i32, i64, i32)\n" +
+            "\"test.op\"(%1, %0) : (i64, i32) -> ()",
+        parser = parser.Block(_)
+      )
+    )
+
+    println(
+      parser.testParse(
+        text =
+          "{^bb0(%5: i32):\n" + "%0, %1, %2 = \"test.op\"() : () -> (i32, i64, i32)\n" +
+            "\"test.op\"(%1, %0) : (i64, i32) -> ()" + "^bb1(%4: i32):\n" + "%7, %8, %9 = \"test.op\"() : () -> (i32, i64, i32)\n" +
+            "\"test.op\"(%8, %7) : (i64, i32) -> ()" + "}",
+        parser = parser.Region(_)
+      )
+    )
+
+    try {
+      println(
+        parser.testParse(
+          text =
+            "{^bb0(%5: i32):\n" + "%0, %1, %2 = \"test.op\"() : () -> (i32, i64, i32)\n" +
+              "\"test.op\"(%1, %0) : (i64, i32) -> ()" + "^bb0(%4: i32):\n" + "%7, %8, %9 = \"test.op\"() : () -> (i32, i64, i32)\n" +
+              "\"test.op\"(%8, %7) : (i64, i32) -> ()" + "}",
+          parser = parser.Region(_)
+        )
+      )
+    } catch {
+      case e: Exception => println(e.getMessage)
+    }
+
+    println(
+      parser.testParse(
+        text = "{}",
+        parser = parser.Region(_)
       )
     )
 
@@ -369,59 +554,3 @@ object Parser {
 /////////////////////
 // VERSION CONTROL //
 /////////////////////
-
-/*
-
-def generateOperation
-
-    var resultss: Seq[Value] = Seq()
-    var operandss: Seq[Value] = Seq()
-
-    for (i <- (0 to results.length - 1)) {
-        val name = results(i)
-        val value = new Value(typ = resultsTypes(i))
-
-        // SSA value cannot be defined twice
-        if (valueMap.contains(name)) {
-            throw new Exception(s"SSA Value cannot be defined twice %$name")
-        }
-
-        valueMap(name) = value
-
-        resultss = resultss :+ value
-    }
-
-    for (i <- (0 to operands.length - 1)) {
-        val name = operands(i)
-        // val value = new Value(typ = operandsTypes(i))
-
-        // used but not defined
-        if (!valueMap.contains(name)) {
-            // valueMap(name) = value
-            throw new Exception(s"SSA value used but not defined %$name")
-        }
-        if (valueMap(name).typ != operandsTypes(i)) {
-            throw new Exception(s"$name use with type ${operandsTypes(i)} but defined with type ${valueMap(name).typ}")
-        }
-        operandss = operandss :+ valueMap(name)
-    }
-
-
-    ///////////
-    // Types //
-    ///////////
-
-    def joinTypeList(types: (Seq[Attribute], Seq[Seq[Attribute]])): Seq[Attribute] = {
-        return types._1 ++ types._2.flatten
-    }
-
-    def mapAttribute(typeName: String): Seq[Attribute] = {
-
-        // if (!typeAttributeMap.contains(typeName)) {
-        //     typeAttributeMap(typeName) = new Attribute(name = typeName)
-        // }
-        return Seq(new Attribute(name = typeName))
-    }
-
-
- */
