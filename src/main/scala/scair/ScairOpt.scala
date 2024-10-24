3fc2f8a820529f0e0c0f1a349e2ef5263defe4b8
Emilien Bauer <bauer.emilien@gmail.com>
2024-10-24 16:20:10 +0100

    Implement `parsing-diagnostics`

--- src/main/scala/scair/ScairOpt.scala
+++ src/main/scala/scair/ScairOpt.scala
@@ -11,6 +11,7 @@ package scair {
       val input: Option[String] = None,
       val skip_verify: Boolean = false,
       val split_input_file: Boolean = false,
+      val parsing_diagnostics: Boolean = false,
       val print_generic: Boolean = false,
       val passes: Seq[String] = Seq()
   )
@@ -37,6 +38,12 @@ package scair {
             .optional()
             .text("Split input file on `// -----`")
             .action((_, c) => c.copy(split_input_file = true)),
+          opt[Unit]("parsing_diagnostics")
+            .optional()
+            .text(
+              "Parsing diagnose mode, i.e parse errors are not fatal for the whole run"
+            )
+            .action((_, c) => c.copy(parsing_diagnostics = true)),
           opt[Unit]('g', "print_generic")
             .optional()
             .text("Print Strictly in Generic format")
@@ -71,41 +78,41 @@ package scair {
 
             // Parse content
             val parser = new scair.Parser(args)
-            val input_module = parser.parseThis(
+            parser.parseThis(
               chunk,
               pattern = parser.TopLevel(_)
             ) match {
-              case fastparse.Parsed.Success(value, _) => value
+              case fastparse.Parsed.Success(input_module, _) =>
+                // verify parsed content
+                if (!skip_verify) input_module.verify()
+
+                // apply the specified passes
+                val transformCtx = new TransformContext()
+                var module = input_module
+                for (name <- passes) {
+                  transformCtx.getPass(name) match {
+                    case Some(pass) =>
+                      module = pass.transform(module)
+                      if (!skip_verify) module.verify()
+                    case None =>
+                  }
+                }
+
+                val printer = new Printer(print_generic)
+                module match {
+                  case x: ModuleOp =>
+                    printer.printOperation(x) // printer.printOperation(x)
+                  case _ =>
+                    throw new Exception(
+                      "Top level module must be the Builtin module of type ModuleOp.\n" +
+                        "==------------------==" +
+                        s"Check your tranformations: ${passes.mkString(", ")}" +
+                        "==------------------=="
+                    )
+                }
               case failure: fastparse.Parsed.Failure =>
                 parser.error(failure)
-            }
 
-            // verify parsed content
-            if (!skip_verify) input_module.verify()
-
-            // apply the specified passes
-            val transformCtx = new TransformContext()
-            var module = input_module
-            for (name <- passes) {
-              transformCtx.getPass(name) match {
-                case Some(pass) =>
-                  module = pass.transform(module)
-                  if (!skip_verify) module.verify()
-                case None =>
-              }
-            }
-
-            val printer = new Printer(print_generic)
-            module match {
-              case x: ModuleOp =>
-                printer.printOperation(x) // printer.printOperation(x)
-              case _ =>
-                throw new Exception(
-                  "Top level module must be the Builtin module of type ModuleOp.\n" +
-                    "==------------------==" +
-                    s"Check your tranformations: ${passes.mkString(", ")}" +
-                    "==------------------=="
-                )
             }
           }
 
---
+++ tests/filecheck/parsing_diagnostics.mlir
@@ -0,0 +1,13 @@
+// RUN: scair-opt --split_input_file --parsing_diagnostics %s | filecheck %s
+
+notir
+// -----
+notir
+
+// CHECK:       Parse error at /home/papychacal/scair/tests/filecheck/parsing_diagnostics.mlir:3:1:
+// CHECK-EMPTY:
+// CHECK-NEXT:  Expected (parse | OperationPat | Pass | end-of-input):3:1, found "notir"
+// CHECK-NEXT:  // -----
+// CHECK-NEXT:  Parse error at /home/papychacal/scair/tests/filecheck/parsing_diagnostics.mlir:1:1:
+// CHECK-EMPTY:
+// CHECK-NEXT:  Expected (Pass | parse | OperationPat | end-of-input):1:1, found "notir\n\n// "
