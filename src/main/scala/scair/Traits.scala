ac220be33068350009b8f910090e81d33a1a7eb0
baymaks <s2071662@ed.ac.uk>
2024-11-05 17:15:22 +0000

    Traits implemented!

---
+++ src/main/scala/scair/Traits.scala
@@ -0,0 +1,43 @@
+package scair.traits
+
+import scair.ir.OpTrait
+
+// ████████╗ ██████╗░ ░█████╗░ ██╗ ████████╗ ░██████╗
+// ╚══██╔══╝ ██╔══██╗ ██╔══██╗ ██║ ╚══██╔══╝ ██╔════╝
+// ░░░██║░░░ ██████╔╝ ███████║ ██║ ░░░██║░░░ ╚█████╗░
+// ░░░██║░░░ ██╔══██╗ ██╔══██║ ██║ ░░░██║░░░ ░╚═══██╗
+// ░░░██║░░░ ██║░░██║ ██║░░██║ ██║ ░░░██║░░░ ██████╔╝
+// ░░░╚═╝░░░ ╚═╝░░╚═╝ ╚═╝░░╚═╝ ╚═╝ ░░░╚═╝░░░ ╚═════╝░
+
+/*≡==--=≡≡≡≡=--=≡≡*\
+||   TERMINATOR   ||
+\*≡==---=≡≡=---==≡*/
+
+trait IsTerminator extends OpTrait {
+  
+  abstract override def trait_verify(): Unit = {
+    op.container_block match {
+      case Some(b) => 
+        if (op != b.operations.last) then 
+          throw new Exception(s"Operation '${op.name}' marked as a terminator, but is not the last operation within its container block") 
+      case None => 
+        throw new Exception(s"Operation '${op.name}' marked as a terminator, but is not contained in any block.")
+    }
+    super.trait_verify()
+  }
+}
+
+/*≡==---=≡≡≡≡≡=---=≡≡*\
+||   NO TERMINATOR   ||
+\*≡==----=≡≡≡=----==≡*/
+
+trait NoTerminator extends OpTrait {
+  
+  abstract override def trait_verify(): Unit = {
+    if (op.regions.filter(x => x.blocks.length != 1).length != 0) then
+      throw new Exception(s"NoTerminator Operation '${op.name}' requires single-block regions")
+
+    super.trait_verify()
+  }
+}
+
