118c5f7668536535426bd43dcae9e644014e59c0
Emilien Bauer <papychacal@gmail.com>
2024-11-19 14:23:42 +0000

    Move allDialects away from core. (#112)

--- src/main/scala/scair/core/MLContext.scala
+++ src/main/scala/scair/core/MLContext.scala
@@ -1,16 +1,8 @@
 package scair
 
-import scair.dialects.CMath.cmath.CMath
-import scair.dialects.LingoDB.TupleStream.TupleStreamDialect
-import scair.dialects.LingoDB.DBOps.DBOps
-import scair.dialects.LingoDB.SubOperatorOps.SubOperatorOps
-import scair.dialects.LingoDB.RelAlgOps.RelAlgOps
-import scair.dialects.affine.Affine
 import scala.collection.mutable
 import scair.ir._
-
-private val allDialects: Seq[Dialect] =
-  Seq(CMath, TupleStreamDialect, DBOps, SubOperatorOps, RelAlgOps, Affine)
+import scair.utils.allDialects
 
 class MLContext() {
 
