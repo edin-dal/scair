d34636d4efe989fd5604db30ed1f2a54625a7d3a
Emilien Bauer <bauer.emilien@gmail.com>
2024-12-09 18:42:58 +0000

    Store/Load.

--- dialects/src/main/scala-3/memref/Memref.scala
+++ dialects/src/main/scala-3/memref/Memref.scala
@@ -15,6 +15,18 @@ case class AllocOp(
 case class DeallocOp(
     memref: Operand[MemrefType]
 ) extends OperationFE
+case class LoadOp(
+    memref: Operand[MemrefType],
+    indices: Variadic[Operand[IndexType.type]],
+    result: Result[MemrefType]
+) extends OperationFE
+case class StoreOp(
+    value: Operand[MemrefType],
+    memref: Operand[MemrefType],
+    indices: Variadic[Operand[IndexType.type]]
+) extends OperationFE
 
 object MemrefGen
-    extends ScaIRDLDialect(summonDialect[(AllocOp, DeallocOp)]("Memref"))
+    extends ScaIRDLDialect(
+      summonDialect[(AllocOp, DeallocOp, LoadOp, StoreOp)]("Memref")
+    )
