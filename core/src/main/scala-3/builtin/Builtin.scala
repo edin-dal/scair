f0cef8595a723bf5cf719f514f2b70351bd39136
Emilien Bauer <bauer.emilien@gmail.com>
2025-07-15 14:35:03 +0100

    Implement `builtin.unrealized_conversion_cast`

--- core/src/main/scala-3/builtin/Builtin.scala
+++ core/src/main/scala-3/builtin/Builtin.scala
@@ -532,4 +532,27 @@ case class ModuleOp(
 
 }
 
-val BuiltinDialect = Dialect(Seq(ModuleOp), Seq())
+object UnrealizedConversionCastOp extends OperationCompanion {
+  override def name = "builtin.unrealized_conversion_cast"
+}
+
+case class UnrealizedConversionCastOp(
+    override val operands: Seq[Value[Attribute]] = Seq(),
+    override val successors: Seq[Block] = Seq(),
+    override val results: Seq[Result[Attribute]] = Seq(),
+    override val regions: Seq[Region] = Seq(),
+    override val properties: Map[String, Attribute] =
+      Map.empty[String, Attribute],
+    override val attributes: DictType[String, Attribute] =
+      DictType.empty[String, Attribute]
+) extends BaseOperation(
+      name = "builtin.unrealized_conversion_cast",
+      operands,
+      successors,
+      results,
+      regions,
+      properties,
+      attributes
+    )
+
+val BuiltinDialect = Dialect(Seq(ModuleOp, UnrealizedConversionCastOp), Seq())
