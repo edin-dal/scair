2bc2b4e67be016f09d1559fdc2658a09852488fa
baymaks <s2071662@ed.ac.uk>
2025-08-04 16:52:47 +0100

    func

--- dialects/src/main/scala-3/func/Func.scala
+++ dialects/src/main/scala-3/func/Func.scala
@@ -5,11 +5,47 @@ import scair.clair.macros.*
 import scair.dialects.builtin.*
 import scair.ir.*
 
+/*≡=--==≡≡≡≡==--=≡*\
+||   INTERFACES   ||
+\*≡=---==≡≡==---=≡*/
+
+trait CallOpInterface
+trait MemRefsNormalizable
+trait SymbolUserOpInterface
+trait DeclareOpInterfaceMethods[T]
+
+/*≡=--==≡≡≡≡==--=≡*\
+||   ATTRIBUTES   ||
+\*≡=---==≡≡==---=≡*/
+
+type UnitAttr = Attribute
+
+/*≡=--==≡≡≡≡==--=≡*\
+||   OPERATIONS   ||
+\*≡=---==≡≡==---=≡*/
+
 case class Call(
-    callee: SymbolRefAttr,
     _operands: Seq[Operand[Attribute]],
-    _results: Seq[Result[Attribute]]
-) extends DerivedOperation["func.call", Call] derives DerivedOperationCompanion
+    _results: Seq[Result[Attribute]],
+    callee: SymbolRefAttr,
+    arg_attrs: Option[ArrayAttribute[DictionaryAttr]],
+    res_attrs: Option[ArrayAttribute[DictionaryAttr]],
+    no_inline: UnitAttr
+) extends DerivedOperation["func.call", Call],
+      CallOpInterface,
+      MemRefsNormalizable,
+      DeclareOpInterfaceMethods[SymbolUserOpInterface]
+    derives DerivedOperationCompanion
+
+case class CallIndirect(
+    _operands: Seq[Operand[Attribute]],
+    _results: Seq[Result[Attribute]],
+    callee: FunctionType,
+    arg_attrs: Option[ArrayAttribute[DictionaryAttr]],
+    res_attrs: Option[ArrayAttribute[DictionaryAttr]],
+    no_inline: UnitAttr
+) extends DerivedOperation["func.call_indirect", CallIndirect],
+      CallOpInterface derives DerivedOperationCompanion
 
 case class Func(
     sym_name: StringData,
