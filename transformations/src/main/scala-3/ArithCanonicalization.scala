34d6bc9035f84548ebc1d21e761a8619cd147d1e
Emilien Bauer <bauer.emilien@gmail.com>
2025-08-05 19:02:28 +0100

    tweak

--- transformations/src/main/scala-3/ArithCanonicalization.scala
+++ transformations/src/main/scala-3/ArithCanonicalization.scala
@@ -9,29 +9,27 @@ import scair.transformations.patterns.*
 // TODO: Generalize in terms of effects and out of dialect
 val RemoveUnusedOperations = pattern {
   case _: IsTerminator => PatternAction.Abort
-  case op: NoMemoryEffect if op.results.forall(_.uses.isEmpty) => PatternAction.Erase
+  case op: NoMemoryEffect if op.results.forall(_.uses.isEmpty) =>
+    PatternAction.Erase
   case op: NoMemoryEffect => PatternAction.Abort
 }
 
 // TODO: Generalize in Commutative/ConstantLike
 val AddICommute = pattern {
-  case AddI(rhs = Owner(_: Constant)) => PatternAction.Abort
-  case AddI(lhs @ Owner(Constant(value = c0: IntegerAttr)), rhs, Result(tpe)) =>
+  case AddI(rhs = Owner(_: Constant))                   => PatternAction.Abort
+  case AddI(lhs @ Owner(_: Constant), rhs, Result(tpe)) =>
     AddI(rhs, lhs, Result(tpe))
 }
 
 // addi(addi(x, c0), c1) -> addi(x, c0 + c1)
 val AddIAddConstant = pattern {
   case AddI(
-        Owner(
-          AddI(x @ Value(tpe), Owner(Constant(c0: IntegerAttr, _)), _)
-        ),
+        Owner(AddI(x, Owner(Constant(c0: IntegerAttr, _)), _)),
         Owner(Constant(c1: IntegerAttr, _)),
         _
       ) =>
-    val cv = Result(tpe)
-    val c = Constant(c0 + c1, cv)
-    Seq(c, AddI(x, cv, Result(tpe)))
+    val cv = Result(x.typ)
+    Seq(Constant(c0 + c1, cv), AddI(x, cv, Result(x.typ)))
 }
 
 object ArithCanonicalize extends ModulePass {
