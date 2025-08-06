b5da8d2cd520b819910447e2441a504c16167f4c
baymaks <s2071662@ed.ac.uk>
2025-08-06 16:02:34 +0100

    scf added

--- dialects/src/main/scala-3/scf/scf.scala
+++ dialects/src/main/scala-3/scf/scf.scala
@@ -4,6 +4,7 @@ import scair.clair.codegen.*
 import scair.clair.macros.*
 import scair.dialects.builtin.*
 import scair.ir.*
+import javax.print.attribute.standard.Destination
 
 // ░██████╗ ░█████╗░ ███████╗
 // ██╔════╝ ██╔══██╗ ██╔════╝
@@ -19,15 +20,32 @@ import scair.ir.*
 // ██████╔╝ ██║ ██║░░██║ ███████╗ ███████╗ ░░██████╔╝ ░░░██║░░░
 // ╚═════╝░ ╚═╝ ╚═╝░░╚═╝ ╚══════╝ ╚══════╝ ░░ ╚════╝░ ░░░╚═╝░░░
 
-/*≡==--==≡≡≡≡≡≡≡≡≡≡≡==--=≡≡*\
-||  TYPES AND CONSTRAINTS  ||
-\*≡==---==≡≡≡≡≡≡≡≡≡==---==≡*/
+/*≡=--==≡≡≡≡==--=≡*\
+||   ATTRIBUTES   ||
+\*≡=---==≡≡==---=≡*/
+
+type DenseI64ArrayAttr = DenseArrayAttr
+type AnyRankedTensor = RankedTensorType
+// TODO: Should be array of "DeviceMappingAttribute", but we're not interested yet.
+type DeviceMappingArrayAttr = ArrayAttribute[Attribute]
+
+/*≡=--==≡≡≡≡≡==--=≡*\
+||   CONSTRAINTS   ||
+\*≡=---==≡≡≡==---=≡*/
 
 // TODO: this needs to be constrained specifically to an I1 integer
 type I1 = IntegerType
 // TODO: this needs to be a signless integer type specifically
 type AnySignlessIntegerOrIndex = IntegerType | IndexType
 type Index = IndexType
+type SizedRegion[V <: Int] = Region
+type MaxSizedRegion[V <: Int] = Region
+// TODO: Variadic regions
+type VariadicRegion[v <: Region] = Region
+
+/*≡=--==≡≡≡≡==--=≡*\
+||   INTERFACES   ||
+\*≡=---==≡≡==---=≡*/
 
 trait AllTypesMatch(values: Attribute*) extends Operation {
 
@@ -45,109 +63,172 @@ trait AllTypesMatch(values: Attribute*) extends Operation {
 
 }
 
+// def GraphRegionNoTerminator : TraitList<[
+//     NoTerminator,
+//     SingleBlock,
+//     RegionKindInterface,
+//     HasOnlyGraphRegion
+//   ]>;
+
+trait HasParent[P <: Operation]
+trait Pure
+trait RegionBranchTerminatorOpInterface
+trait RegionBranchOpInterface
+trait AutomaticAllocationScope
+trait LoopLikeOpInterface
+trait ConditionallySpeculable
+trait SingleBlockImplicitTerminator[T <: Operation]
+trait RecursiveMemoryEffects
+trait DestinationStyleOpInterface
+trait HasParallelRegion
+trait AttrSizedOperandSegments
+trait ParallelCombiningOpInterface
+trait RecursivelySpeculatable
+trait NoRegionArguments
+trait InferTypeOpAdaptor
+trait SingleBlock
+trait ReturnLike
+trait ParentOneOf[Operations]
+
 /*≡==--==≡≡≡≡≡≡≡≡≡==--=≡≡*\
 ||  OPERATION DEFINTION  ||
 \*≡==---==≡≡≡≡≡≡≡==---==≡*/
 
 case class Condition(
-    condition: Operand[I1],
-    args: Seq[Operand[Attribute]]
-) extends DerivedOperation["scf.condition", Condition]
-    with NoMemoryEffect
-    with IsTerminator derives DerivedOperationCompanion
+    val condition: Operand[I1],
+    val args: Seq[Operand[Attribute]]
+) extends DerivedOperation["scf.condition", Condition],
+      NoMemoryEffect,
+      HasParent[WhileOp],
+      RegionBranchTerminatorOpInterface,
+      IsTerminator derives DerivedOperationCompanion
 
 case class ExecuteRegionOp(
-    region: Region,
-    result: Seq[Result[Attribute]]
-) extends DerivedOperation["scf.execute_region", ExecuteRegionOp]
-    derives DerivedOperationCompanion
+    val region: Region,
+    val result: Seq[Result[TypeAttribute]]
+) extends DerivedOperation["scf.execute_region", ExecuteRegionOp],
+      RegionBranchOpInterface derives DerivedOperationCompanion
 
-// TODO: this should also contain a SingleBlockImplicitTerminator<"scf::YieldOp">,
 case class ForOp(
-    lowerBound: Operand[AnySignlessIntegerOrIndex],
-    upperBound: Operand[AnySignlessIntegerOrIndex],
-    step: Operand[AnySignlessIntegerOrIndex],
-    initArgs: Seq[Operand[Attribute]],
-    region: Region,
-    resultss: Seq[Result[Attribute]]
-) extends DerivedOperation["scf.for", ForOp]
-    with AllTypesMatch(lowerBound.typ, upperBound.typ, step.typ)
-    derives DerivedOperationCompanion
+    val lowerBound: Operand[AnySignlessIntegerOrIndex],
+    val upperBound: Operand[AnySignlessIntegerOrIndex],
+    val step: Operand[AnySignlessIntegerOrIndex],
+    val initArgs: Seq[Operand[TypeAttribute]],
+    val _results: Seq[Result[TypeAttribute]],
+    val region: SizedRegion[1]
+) extends DerivedOperation["scf.for", ForOp],
+      AutomaticAllocationScope,
+      LoopLikeOpInterface,
+      AllTypesMatch(lowerBound.typ, upperBound.typ, step.typ),
+      ConditionallySpeculable,
+      RegionBranchOpInterface,
+      SingleBlockImplicitTerminator[YieldOp],
+      RecursiveMemoryEffects derives DerivedOperationCompanion
 
 case class ForallOp(
-    dynamicLowerBound: Seq[Operand[Index]],
-    dynamicUpperBound: Seq[Operand[Index]],
-    dynamicStep: Seq[Operand[Index]],
-    staticLowerBound: DenseArrayAttr,
-    staticUpperBound: DenseArrayAttr,
-    staticStep: DenseArrayAttr,
-    outputs: Seq[Operand[RankedTensorType]],
-    // TODO: Should be array of "DeviceMappingAttribute", but we're not interested yet.
-    mapping: Option[ArrayAttribute[Attribute]],
-    region: Region,
-    resultss: Seq[Result[Attribute]]
-) extends DerivedOperation["scf.forall", ForallOp]
-    derives DerivedOperationCompanion
+    val dynamicLowerBound: Seq[Operand[Index]],
+    val dynamicUpperBound: Seq[Operand[Index]],
+    val dynamicStep: Seq[Operand[Index]],
+    val staticLowerBound: DenseI64ArrayAttr,
+    val staticUpperBound: DenseI64ArrayAttr,
+    val staticStep: DenseI64ArrayAttr,
+    val outputs: Seq[Operand[AnyRankedTensor]],
+    val mapping: Option[DeviceMappingArrayAttr],
+    val _results: Seq[Result[TypeAttribute]],
+    val region: SizedRegion[1]
+) extends DerivedOperation["scf.forall", ForallOp],
+      AttrSizedOperandSegments,
+      AutomaticAllocationScope,
+      LoopLikeOpInterface,
+      RecursiveMemoryEffects,
+      SingleBlockImplicitTerminator[InParallelOp],
+      RegionBranchOpInterface,
+      DestinationStyleOpInterface,
+      HasParallelRegion derives DerivedOperationCompanion
 
+// TODO: GraphRegionNoTerminator
 case class InParallelOp(
-    region: Region
-) extends DerivedOperation["scf.forall.in_parallel", InParallelOp]
-    with IsTerminator
-    with NoMemoryEffect derives DerivedOperationCompanion
+    val region: SizedRegion[1]
+) extends DerivedOperation["scf.forall.in_parallel", InParallelOp],
+      IsTerminator,
+      NoMemoryEffect,
+      ParallelCombiningOpInterface,
+      HasParent[ForallOp] derives DerivedOperationCompanion
 
 case class IfOp(
-    condition: Operand[I1],
-    thenRegion: Region,
-    elseRegion: Region,
-    resultss: Seq[Result[Attribute]]
-) extends DerivedOperation["scf.if", IfOp] derives DerivedOperationCompanion
+    val condition: Operand[I1],
+    val thenRegion: SizedRegion[1],
+    val elseRegion: MaxSizedRegion[1],
+    val _results: Seq[Result[TypeAttribute]]
+) extends DerivedOperation["scf.if", IfOp],
+      RegionBranchOpInterface,
+      InferTypeOpAdaptor,
+      SingleBlockImplicitTerminator[YieldOp],
+      RecursiveMemoryEffects,
+      RecursivelySpeculatable,
+      NoRegionArguments derives DerivedOperationCompanion
 
 case class ParallelOp(
-    lowerBound: Seq[Operand[Index]],
-    upperBound: Seq[Operand[Index]],
-    step: Seq[Operand[Index]],
-    initVals: Seq[Operand[Attribute]],
-    region: Region,
-    resultss: Seq[Result[Attribute]]
-) extends DerivedOperation["scf.parallel", ParallelOp]
-    derives DerivedOperationCompanion
+    val lowerBound: Seq[Operand[Index]],
+    val upperBound: Seq[Operand[Index]],
+    val step: Seq[Operand[Index]],
+    val initVals: Seq[Operand[TypeAttribute]],
+    val _results: Seq[Result[TypeAttribute]],
+    val region: SizedRegion[1]
+) extends DerivedOperation["scf.parallel", ParallelOp],
+      AutomaticAllocationScope,
+      AttrSizedOperandSegments,
+      LoopLikeOpInterface,
+      RecursiveMemoryEffects,
+      RegionBranchOpInterface,
+      SingleBlockImplicitTerminator[ReduceOp],
+      HasParallelRegion derives DerivedOperationCompanion
 
 case class ReduceOp(
-    operandss: Seq[Operand[Attribute]],
-    // TODO: variadic regions
-    reductions: Region
-) extends DerivedOperation["scf.reduce", ReduceOp]
-    with IsTerminator derives DerivedOperationCompanion
+    val _operands: Seq[Operand[Attribute]],
+    val reductions: VariadicRegion[SizedRegion[1]]
+) extends DerivedOperation["scf.reduce", ReduceOp],
+      HasParent[ParallelOp],
+      RecursiveMemoryEffects,
+      RegionBranchTerminatorOpInterface,
+      IsTerminator derives DerivedOperationCompanion
 
 case class ReduceReturnOp(
-    resultss: Result[Attribute]
-) extends DerivedOperation["scf.reduce.return", ReduceReturnOp]
-    with IsTerminator
-    with NoMemoryEffect derives DerivedOperationCompanion
+    val _results: Result[TypeAttribute]
+) extends DerivedOperation["scf.reduce.return", ReduceReturnOp],
+      HasParent[ReduceOp],
+      NoMemoryEffect,
+      IsTerminator derives DerivedOperationCompanion
 
 case class WhileOp(
-    inits: Seq[Operand[Attribute]],
-    before: Region,
-    after: Region,
-    resultss: Seq[Result[Attribute]]
-) extends DerivedOperation["scf.while", WhileOp]
-    derives DerivedOperationCompanion
+    val inits: Seq[Operand[TypeAttribute]],
+    val before: SizedRegion[1],
+    val after: SizedRegion[1],
+    val _results: Seq[Result[TypeAttribute]]
+) extends DerivedOperation["scf.while", WhileOp],
+      RegionBranchOpInterface,
+      LoopLikeOpInterface,
+      RecursiveMemoryEffects,
+      SingleBlock derives DerivedOperationCompanion
 
 case class IndexSwitchOp(
-    arg: Operand[Index],
-    cases: Operand[DenseArrayAttr],
-    defaultRegion: Region,
-    // TODO: variadic regions
-    caseRegions: Region,
-    resultss: Seq[Result[Attribute]]
-) extends DerivedOperation["scf.index_switch", IndexSwitchOp]
-    derives DerivedOperationCompanion
+    val arg: Operand[Index],
+    val cases: DenseI64ArrayAttr,
+    val defaultRegion: SizedRegion[1],
+    val caseRegions: VariadicRegion[SizedRegion[1]],
+    val _results: Seq[Result[TypeAttribute]]
+) extends DerivedOperation["scf.index_switch", IndexSwitchOp],
+      RecursiveMemoryEffects,
+      SingleBlockImplicitTerminator[YieldOp],
+      RegionBranchOpInterface derives DerivedOperationCompanion
 
 case class YieldOp(
-    resultss: Seq[Operand[Attribute]]
-) extends DerivedOperation["scf.yield", YieldOp]
-    with IsTerminator
-    with NoMemoryEffect derives DerivedOperationCompanion
+    val _results: Seq[Operand[TypeAttribute]]
+) extends DerivedOperation["scf.yield", YieldOp],
+      ParentOneOf[(ExecuteRegionOp, ForOp, IfOp, IndexSwitchOp, WhileOp)],
+      ReturnLike,
+      IsTerminator,
+      NoMemoryEffect derives DerivedOperationCompanion
 
 val SCFDialect =
   summonDialect[
