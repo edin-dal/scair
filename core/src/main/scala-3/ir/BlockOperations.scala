2ca2bde7a484cd2e9490533ceacc67260ff2458c
Emilien Bauer <bauer.emilien@gmail.com>
2025-08-25 16:05:15 +0100

    BLockOperations side tweaks.

--- core/src/main/scala-3/ir/BlockOperations.scala
+++ core/src/main/scala-3/ir/BlockOperations.scala
@@ -19,44 +19,29 @@ object BlockOperations:
 
 class BlockOperations extends IntrusiveList[Operation]:
 
-  private final def handleOperationInsertion(op: Operation) =
+  private inline def handleOperationInsertion(op: Operation) =
     op.operands.zipWithIndex.foreach((o, i) => o.uses += Use(op, i))
 
-  private final def handleOperationRemoval(op: Operation) =
+  private inline def handleOperationRemoval(op: Operation) =
     op.operands.zipWithIndex.foreach((o, i) =>
       o.uses.filterInPlace(_.operation != op)
     )
 
   override final def addOne(elem: Operation): this.type =
-    super.addOne(elem)
     handleOperationInsertion(elem)
-    this
+    super.addOne(elem)
 
   override final def prepend(elem: Operation): this.type =
-    super.prepend(elem)
     handleOperationInsertion(elem)
-    this
+    super.prepend(elem)
 
   override final def insert(v: Operation, elem: Operation): Unit =
     super.insert(v, elem)
     handleOperationInsertion(elem)
 
-  override final def insertAll(
-      idx: Operation,
-      elems: IterableOnce[Operation]
-  ): Unit =
-    super.insertAll(idx, elems)
-    elems.foreach(handleOperationInsertion)
-
-  override final def remove(idx: Int): Operation =
-    val op = super.remove(idx)
-    handleOperationRemoval(op)
-    op
-
   override final def subtractOne(elem: Operation): this.type =
-    val op = super.subtractOne(elem)
     handleOperationRemoval(elem)
-    this
+    super.subtractOne(elem)
 
   override final def update(v: Operation, elem: Operation): Unit =
     handleOperationRemoval(v)
