81045ae0ea4ba96930aa9b5d6d9ae6bf31a31e89
Emilien Bauer <bauer.emilien@gmail.com>
2025-08-27 15:28:18 +0100

    Flip the conversion, do the same trick for Block Arguments.

--- core/src/main/scala-3/ir/Value.scala
+++ core/src/main/scala-3/ir/Value.scala
@@ -46,6 +46,13 @@ opaque type Result[+T <: Attribute] <: Value[T] = Value[T]
 object Result:
   def apply[T <: Attribute](typ: T): Result[T] = Value(typ)
   def unapply[T <: Attribute](r: Result[T]): Some[T] = Some(r.typ)
-  given [T <: Attribute] => Conversion[Result[T], Value[T]] = identity
+  given [T <: Attribute] => Conversion[Value[T], Result[T]] = identity
+
+opaque type BlockArgument[+T <: Attribute] <: Value[T] = Value[T]
+
+object BlockArgument:
+  def apply[T <: Attribute](typ: T): BlockArgument[T] = Value(typ)
+  def unapply[T <: Attribute](r: BlockArgument[T]): Some[T] = Some(r.typ)
+  given [T <: Attribute] => Conversion[Value[T], BlockArgument[T]] = identity
 
 extension (seq: Seq[Value[Attribute]]) def typ: Seq[Attribute] = seq.map(_.typ)
