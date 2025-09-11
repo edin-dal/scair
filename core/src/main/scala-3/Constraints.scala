19b477f6d40d5536be286fb69a254963a10cbe46
Maks Kret <s2071662@ed.ac.uk>
2025-09-11 16:39:27 +0100

    VarDef Constraint Implementation (#331)
    ```Scala
    case class MulFVar(
        lhs: Operand[FloatType !> Var["T"]],
        rhs: Operand[FloatType !> Var["T"]],
        result: Result[FloatType]
    ) extends DerivedOperation["cmath.mul", MulFVar]
    ```
    - VarDef constraint allows for adding constraints between different
    Operands, where operand 'b' binds its type to operand 'a'.

--- core/src/main/scala-3/Constraints.scala
+++ core/src/main/scala-3/Constraints.scala
@@ -2,19 +2,50 @@ package scair.core.constraints
 
 import scair.ir.*
 
+import scala.compiletime.constValue
 import scala.quoted.*
+
+// ░█████╗░ ░█████╗░ ███╗░░██╗ ░██████╗ ████████╗ ██████╗░ ░█████╗░ ██╗ ███╗░░██╗ ████████╗ ░██████╗
+// ██╔══██╗ ██╔══██╗ ████╗░██║ ██╔════╝ ╚══██╔══╝ ██╔══██╗ ██╔══██╗ ██║ ████╗░██║ ╚══██╔══╝ ██╔════╝
+// ██║░░╚═╝ ██║░░██║ ██╔██╗██║ ╚█████╗░ ░░░██║░░░ ██████╔╝ ███████║ ██║ ██╔██╗██║ ░░░██║░░░ ╚█████╗░
+// ██║░░██╗ ██║░░██║ ██║╚████║ ░╚═══██╗ ░░░██║░░░ ██╔══██╗ ██╔══██║ ██║ ██║╚████║ ░░░██║░░░ ░╚═══██╗
+// ╚█████╔╝ ╚█████╔╝ ██║░╚███║ ██████╔╝ ░░░██║░░░ ██║░░██║ ██║░░██║ ██║ ██║░╚███║ ░░░██║░░░ ██████╔╝
+// ░╚════╝░ ░╚════╝░ ╚═╝░░╚══╝ ╚═════╝░ ░░░╚═╝░░░ ╚═╝░░╚═╝ ╚═╝░░╚═╝ ╚═╝ ╚═╝░░╚══╝ ░░░╚═╝░░░ ╚═════╝░
+
+/*≡==--==≡≡≡≡≡≡≡≡≡≡≡≡==--=≡≡*\
+||     TOP LEVEL TRAITS     ||
+\*≡==---==≡≡≡≡≡≡≡≡≡≡==---==≡*/
 
 trait Constraint
 
-infix type !>[A <: Attribute, C <: Constraint] = A
+class ConstraintContext() {
 
-trait EqAttr[To <: Attribute] extends Constraint
+  val var_constraints: DictType[String, Attribute] =
+    DictType.empty[String, Attribute]
+
+}
 
 trait ConstraintImpl[c <: Constraint] {
 
-  def verify(attr: Attribute): Either[String, Unit]
+  def verify(attr: Attribute)(using
+      ctx: ConstraintContext
+  ): Either[String, Unit]
 
 }
+
+infix type !>[A <: Attribute, C <: Constraint] = A
+
+/*≡==--==≡≡≡≡≡==--=≡≡*\
+||    CONSTRAINTS    ||
+\*≡==---==≡≡≡==---==≡*/
+
+trait EqAttr[To <: Attribute] extends Constraint
+
+trait Var[To <: String] extends Constraint
+
+/*≡==--==≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡==--=≡≡*\
+||    CONSTRAINTIMPL GIVENS    ||
+\*≡==---==≡≡≡≡≡≡≡≡≡≡≡≡≡==---==≡*/
 
 inline def eqAttr[To <: Attribute]: To =
   ${ eqAttrImpl[To] }
@@ -22,7 +53,27 @@ inline def eqAttr[To <: Attribute]: To =
 inline given [To <: Attribute] => ConstraintImpl[EqAttr[To]] =
   val ref = eqAttr[To]
   new ConstraintImpl {
-    override def verify(attr: Attribute): Either[String, Unit] =
+    override def verify(attr: Attribute)(using
+        ctx: ConstraintContext
+    ): Either[String, Unit] =
       if (attr == ref) Right(())
       else Left(s"Expected ${ref}, got ${attr}")
+  }
+
+inline given [To <: String] => ConstraintImpl[Var[To]] =
+  val name: String = constValue[To]
+  new ConstraintImpl {
+    override def verify(attr: Attribute)(using
+        ctx: ConstraintContext
+    ): Either[String, Unit] = {
+      if (ctx.var_constraints.contains(name)) {
+        if (ctx.var_constraints.apply(name) != attr) then
+          Left(s"Expected ${ctx.var_constraints.apply(name)}, got ${attr}")
+        else Right(())
+      } else {
+        ctx.var_constraints += ((name, attr))
+        Right(())
+      }
+    }
+
   }
