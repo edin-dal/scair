f68ace9591f0a528e53f304af4d7de6c944382d0
Gabayoyo <148140627+Gabayoyo@users.noreply.github.com>
2025-11-01 18:47:54 +0000

    IR Interpretation and evaluation (#347)

---
+++ tests/filecheck/interpreter/arith/constants.mlir
@@ -0,0 +1,10 @@
+// RUN: scair-run %s | filecheck %s
+
+builtin.module {
+  func.func @main() -> (i64) {
+    %0 = "arith.constant"() <{value = 42 : i64}> : () -> (i64)
+    func.return %0 : i64
+  }
+}
+
+// CHECK: 42
---
+++ tools/src/main/scala-3/Evaluator.scala
@@ -0,0 +1,16 @@
+package scair.tools
+
+import scair.ir.*
+
+// TODO: move all interpreter helper functions here
+// match owner of operation to its evaluated value and return it
+
+def lookup_op(operation: Operation | Block, ctx: InterpreterCtx): Attribute =
+  operation match
+    case op: Operation =>
+      ctx.vars.getOrElse(
+        op,
+        throw new Exception(s"Operation ${op} not found in context")
+      )
+    case block: Block =>
+      throw new Exception("Block lookup not supported yet")
