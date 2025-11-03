7ed3ac7fc98236709debbe75160152c2683489a9
Gabayoyo <aarongaba05@gmail.com>
2025-11-03 22:17:55 +0000

    preparation and changes for moving to shapedArray representation of memory

--- tools/src/main/scala-3/MemoryHandler.scala
+++ tools/src/main/scala-3/MemoryHandler.scala
@@ -1,8 +1,5 @@
 package scair.tools
 
-import scair.dialects.builtin.I32
-import scair.dialects.builtin.IntData
-import scair.dialects.builtin.IntegerAttr
 import scair.dialects.memref
 import scala.collection.mutable.ListBuffer
 
@@ -11,21 +8,21 @@ trait MemoryHandler:
 
   // assign some attribute to variables
   def allocate_memory(alloc_op: memref.Alloc, ctx: InterpreterCtx): Unit =
-    // TODO: expand for other types
-    val allocBuffer = ListBuffer()
-    ctx.vars.put(alloc_op, allocBuffer)
+    // TODO: consider 3d array and indices
+    val dynamic_size = alloc_op.dynamicSizes
+    ctx.vars.put(alloc_op, ListBuffer())
 
-  def store_memory(store_op: memref.Store, ctx: InterpreterCtx): Unit = 0
-    // val valueOperand = store_op.value.owner.getOrElse(
-    //   throw new Exception("value operand for store operation not found")
-    // )
-    // val valueToStore = lookup_op(valueOperand, ctx)
-    // val memoryOperand = store_op.memref.owner.getOrElse(
-    //   throw new Exception("memref operand for store operation not found")
-    // )
-// 
-    // // lookup operand of memory to find value needed for address
-    // val addr = lookup_op(memoryOperand, ctx)
+  def store_memory(store_op: memref.Store, ctx: InterpreterCtx): Unit = 
+    val value_operand = store_op.value.owner.getOrElse(
+      throw new Exception("value operand for store operation not found")
+    )
+    val value_to_store = lookup_op(value_operand, ctx)
+    
+    val memory_operand = store_op.memref.owner.getOrElse(
+      throw new Exception("memref operand for store operation not found")
+    )
+    // the list buffer
+    val memref = lookup_op(memory_operand, ctx)
     // addr match
     //   case integerAddr: IntegerAttr =>
     //     val memAddrIndex = integerAddr.value.value.toInt
