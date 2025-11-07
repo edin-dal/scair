24bdeed538f2d200a84a4ba313ac8cec793ba790
Emilien Bauer <bauer.emilien@gmail.com>
2025-11-07 16:56:24 +0000

    temp

--- macros/src/main/scala-3/PartialFunction.scala
+++ macros/src/main/scala-3/PartialFunction.scala
@@ -1,12 +1,14 @@
 package scair.macros
 
 import scala.quoted.*
-import scala.jdk.FunctionWrappers
 
 def popPFImpl(using Quotes) =
-  import quotes.reflect._
+  import quotes.reflect.*
 
-  val methodTpe = MethodType(MethodTypeKind.Plain)(List("whatever"))(_ => List(TypeRepr.of[Any]), _ => TypeRepr.of[String])
+  val methodTpe = MethodType(MethodTypeKind.Plain)(List("whatever"))(
+    _ => List(TypeRepr.of[Any]),
+    _ => TypeRepr.of[String]
+  )
 
   val sym = Symbol.newMethod(
     Symbol.spliceOwner,
@@ -14,50 +16,90 @@ def popPFImpl(using Quotes) =
     methodTpe
   )
 
-  val xSym = Symbol.newVal(Symbol.spliceOwner, "x", TypeRepr.of[Any], Flags.EmptyFlags, Symbol.noSymbol)
+  val xSym = Symbol.newVal(
+    Symbol.spliceOwner,
+    "x",
+    TypeRepr.of[Any],
+    Flags.EmptyFlags,
+    Symbol.noSymbol
+  )
 
   val defdef = DefDef(
     sym,
-    p => Some(Match(Ref(p.head.head.symbol), List(CaseDef(Wildcard(), None, Literal(StringConstant("Hello world!"))))))
+    p =>
+      Some(
+        Match(
+          Ref(p.head.head.symbol),
+          List(
+            CaseDef(Wildcard(), None, Literal(StringConstant("Hello world!")))
+          )
+        )
+      )
   )
 
-
-  Block(List(defdef), Ref(defdef.symbol).etaExpand(Symbol.spliceOwner)).asExprOf[Any => String]
-
-  
+  Block(List(defdef), Ref(defdef.symbol).etaExpand(Symbol.spliceOwner))
+    .asExprOf[Any => String]
 
 inline def popPF = ${ popPFImpl }
 
-def andThenApply[A : Type, B: Type](using Quotes)(a: quotes.reflect.Term, b: Expr[A => B])(using Quotes) =
-  import quotes.reflect._
-  '{$b(${a.asExprOf[A]})}.asTerm
+def mapInlineApply[A: Type, B: Type](using
+    Quotes
+)(a: quotes.reflect.Term, b: Expr[A => B])(using Quotes) =
+  import quotes.reflect.*
+  '{ $b(${ a.asExprOf[A] }) }.asTerm
 
-def andThenTerm[A : Type, B: Type](using Quotes)(a: quotes.reflect.Term, b: Expr[A => B]): quotes.reflect.Term =
-  import quotes.reflect._
+def mapInlineCase[A: Type, B: Type](using
+    Quotes
+)(a: quotes.reflect.CaseDef, b: Expr[A => B]) =
+  import quotes.reflect.*
   a match
-    case Literal(_) =>
-      andThenApply(a, b)
+    case CaseDef(pat, guard, rhs) =>
+      CaseDef(pat, guard, mapInlineTerm(rhs, b))
+
+def mapInlineTerm[A: Type, B: Type](using
+    Quotes
+)(a: quotes.reflect.Term, b: Expr[A => B]): quotes.reflect.Term =
+  import quotes.reflect.*
+  a match
+    case Block(statements, term) =>
+      Block(statements, mapInlineTerm(term, b))
     case Inlined(what, defs, term) =>
-      Inlined(what, defs, andThenTerm(term, b))
+      Inlined(what, defs, mapInlineTerm(term, b))
     case If(cond, thent, elset) =>
-      If(cond, andThenTerm(thent, b),  andThenTerm(elset, b))
+      If(cond, mapInlineTerm(thent, b), mapInlineTerm(elset, b))
+    case Match(selector, cases) =>
+      Match(selector, cases.map(mapInlineCase(_, b)))
+    // TODO: this might be a brittle case as-is
+    case Typed(term, tpt) =>
+      mapInlineTerm(term, b)
+    case Return(term, from) =>
+      Return(mapInlineTerm(term, b), from)
+    case Try(body, catches, finallizer) =>
+      Try(
+        mapInlineTerm(body, b),
+        catches.map(mapInlineCase(_, b)),
+        finallizer
+      )
 
-def andThenImpl[A : Type, B: Type](a: Expr[A], b: Expr[A => B])(using Quotes): Expr[B] =
-  import quotes.reflect._
-  
-  andThenTerm(a.asTerm, b).etaExpand(Symbol.spliceOwner).asExprOf[B]
+    // Was unsure abut going inside Closure?
+    case _ =>
+      mapInlineApply(a, b)
 
-inline def andThen[A, B](inline a: A, inline b: A => B): B =
-  ${ andThenImpl('a, 'b) }
+def mapInlineImpl[A: Type, B: Type](a: Expr[A], b: Expr[A => B])(using
+    Quotes
+): Expr[B] =
+  import quotes.reflect.*
+
+  mapInlineTerm(a.asTerm, b).asExprOf[B]
+
+inline def mapInline[A, B](inline a: A, inline b: A => B): B =
+  ${ mapInlineImpl('a, 'b) }
 
 inline def code(inline expr: Any): String =
   ${ codeImpl('expr) }
 
 def codeImpl(expr: Expr[Any])(using Quotes): Expr[String] =
   Expr(expr.show)
-  
-
-
 
 // inline def combinePF[A, B](inline pfs: PartialFunction[A, B]*) =
 //   ${ combinePFImpl('pfs) }
@@ -67,38 +109,36 @@ def codeImpl(expr: Expr[Any])(using Quotes): Expr[String] =
 
 //   val head = pfs match
 //     case Varargs(Seq(head, _*)) => head
-  
+
 //   head.asTerm match
 //     case Block(List(d@DefDef(name, paramss, tpt, Some(Match(selector, cases)))), e) =>
 //       report.info(s"head: ${head.show}\nparamss: ${paramss}\ntpt: ${tpt.show}\nselector: ${selector.show}\ncases: ${cases.map(_.show).mkString}\nexpr: ${e.show}\nd: ${d.show}")
 
-
-
 //   '{()}
 
-  // Build: new PartialFunction[A, B] { def apply(x: A) = x match { ... }; def isDefinedAt(x: A) = ... }
-  // val aSym = Symbol.newVal(Symbol.spliceOwner, "x", TypeRepr.of[A], Flags.EmptyFlags, Symbol.noSymbol)
-  // val aRef = Ref(aSym)
+// Build: new PartialFunction[A, B] { def apply(x: A) = x match { ... }; def isDefinedAt(x: A) = ... }
+// val aSym = Symbol.newVal(Symbol.spliceOwner, "x", TypeRepr.of[A], Flags.EmptyFlags, Symbol.noSymbol)
+// val aRef = Ref(aSym)
 
-  // val matchExpr = Match(aRef, allCases)
+// val matchExpr = Match(aRef, allCases)
 
-  // // Build an efficient isDefinedAt by reusing pattern structure
-  // val isDefinedAtCases = allCases.map {
-  //   case CaseDef(pat, guard, _) => CaseDef(pat, guard, Literal(BooleanConstant(true)))
-  // }
-  // val isDefinedAtMatch = Match(aRef, isDefinedAtCases :+ CaseDef(Wildcard(), None, Literal(BooleanConstant(false))))
+// // Build an efficient isDefinedAt by reusing pattern structure
+// val isDefinedAtCases = allCases.map {
+//   case CaseDef(pat, guard, _) => CaseDef(pat, guard, Literal(BooleanConstant(true)))
+// }
+// val isDefinedAtMatch = Match(aRef, isDefinedAtCases :+ CaseDef(Wildcard(), None, Literal(BooleanConstant(false))))
 
-  // val pfExpr =
-  //   '{
-  //     new PartialFunction[A, B] {
-  //       def apply(x: A): B = ${ matchExpr.asExprOf[B] }
-  //       def isDefinedAt(x: A): Boolean = ${ isDefinedAtMatch.asExprOf[Boolean] }
-  //     }
-  //   }
+// val pfExpr =
+//   '{
+//     new PartialFunction[A, B] {
+//       def apply(x: A): B = ${ matchExpr.asExprOf[B] }
+//       def isDefinedAt(x: A): Boolean = ${ isDefinedAtMatch.asExprOf[Boolean] }
+//     }
+//   }
 
-  // pfExpr
+// pfExpr
 
-  // reduced
+// reduced
 
 // private def reducePFs[A: Type, B: Type](pfs: Seq[Expr[PartialFunction[A, B]]])(using Quotes): Expr[PartialFunction[A, B]] =
 //   import quotes.reflect.*
@@ -112,9 +152,7 @@ def codeImpl(expr: Expr[Any])(using Quotes): Expr[String] =
 //               Block(List(DefDef(Symbol.newMethod(Symbol.spliceOwner.asInstanceOf[Symbol], name+"$"), tpt.tpe), _ => Some(Match(selector, cases ++ cases2))), e).asExprOf[PartialFunction[A, B]]
 
 //       reducePFs(head +: tail)
-          
-      
-  
+
 // private def extractCases[A: Type, B: Type](pf: Expr[PartialFunction[A, B]])(using Quotes): Seq[quotes.reflect.CaseDef] =
 //   import quotes.reflect.*
 //   pf.asTerm.underlyingArgument match
--- macros/test/src/scala-3/PartialFunction.scala
+++ macros/test/src/scala-3/PartialFunction.scala
@@ -1,20 +1,80 @@
-import org.scalatest._
-import verbs._
-import matchers._
-import flatspec._
+import org.scalatest.*
+import verbs.*
+import matchers.*
+import flatspec.*
 
-import scair.macros.{popPF, andThen, andThenImpl, code}
+import scair.macros.{popPF, mapInline, code}
+import org.scalactic.exceptions.NullArgumentException
 
-inline def p1(x:Int) = x+1
+inline def p1(x: Int) = x + 1
+
 class PartialFunctionTest extends AnyFlatSpec with should.Matchers:
 
-    "popPF" should "pop a PF" in:
-        popPF(0) should be("Hello world!")
-        popPF("anything") should be("Hello world!")
+  "popPF" should "pop a PF" in:
+    popPF(0) should be("Hello world!")
+    popPF("anything") should be("Hello world!")
 
-    "andThen" should "do its thing" in:
-        code(andThen(1, _+1)) should be("(2: scala.Int)")
-        code(andThen(2, p1)) should be("((3: scala.Int): scala.Int)")
-        
-        val b : Boolean = true
-        code(andThen(if b then 1 else 0, _ + 10)) should be("(if (b) 11 else 10: scala.Int)")
+  "mapInline" should "do its thing" in:
+    code(mapInline(1, _ + 1)) should be("(2: scala.Int)")
+    code(mapInline(2, p1)) should be("((3: scala.Int): scala.Int)")
+
+    val b: Boolean = true
+    val i: Int = 42
+    code(mapInline(if b then 1 else 0, _ + 10)) should be(
+      "(if (b) 11 else 10: scala.Int)"
+    )
+    code(
+      mapInline(
+        b match
+          case true  => 1
+          case false => 0,
+        _ + 100
+      )
+    ) should be("""
+(b match {
+  case true =>
+    101
+  case false =>
+    100
+}: scala.Int)
+""".strip())
+    code(
+      mapInline(
+        b match
+          case true  => 1
+          case false => i,
+        _ + 100
+      )
+    ) should be("""
+(b match {
+  case true =>
+    101
+  case false =>
+    i.+(100)
+}: scala.Int)
+""".strip())
+    code(
+      mapInline(
+        try
+          throw new Exception("fail")
+          3
+        catch
+          case _: NullArgumentException => 0
+          case _: Exception             => scala.Range(42, 84).sum
+        ,
+        _ + 100
+      )
+    ) should be("""
+(try {
+  throw new scala.Exception("fail")
+  103
+} catch {
+  case _: org.scalactic.exceptions.NullArgumentException =>
+    100
+  case _: scala.Exception =>
+    {
+      val _$5: scala.Int = scala.Range.apply(42, 84).sum[scala.Int](scala.math.Numeric.IntIsIntegral)
+      _$5.+(100)
+    }
+}: scala.Int)
+""".strip())
