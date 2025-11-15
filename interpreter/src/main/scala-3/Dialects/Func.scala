38a29f384cfdb64d70ffd81f1f26b53b81042a01
Gabayoyo <aarongaba05@gmail.com>
2025-11-15 19:34:14 +0000

    Func implemented, needs testing, need full linking to interpreter file

--- interpreter/src/main/scala-3/Dialects/Func.scala
+++ interpreter/src/main/scala-3/Dialects/Func.scala
@@ -1,6 +1,8 @@
 package scair.tools
 
 import scair.dialects.func
+import scair.ir.Result
+import scair.dialects.builtin.SymbolRefAttr
 
 // assume one return value for now
 object run_return extends OpImpl[func.Return]:
@@ -16,12 +18,31 @@ object run_call extends OpImpl[func.Call]:
           interpreter.interpret_op(op, new_ctx) // create clone so function can run without modifying saved context
         ctx.vars.put(op._results.head, new_ctx.result.get) // assuming one return value for now
 
+object run_function extends OpImpl[func.Func]:
+  def run(op: func.Func, interpreter: Interpreter, ctx: RuntimeCtx): Unit =
+    if op.sym_name.stringLiteral == "main" then
+      val main_ctx = FunctionCtx(
+        name = op.sym_name,
+        body = op.body.blocks.head,
+        saved_ctx = ctx
+      )
+      ctx.funcs.append(main_ctx)
+      val new_call = func.Call(
+        callee = SymbolRefAttr(op.sym_name),
+        _operands = Seq(),
+        _results = op.function_type.outputs.map(res => Result(res))
+      )
+      run_call.run(new_call, interpreter, ctx)
+    else
+      ctx.add_func_ctx(op)
+
         
 
 
 val InterpreterFuncDialect = summonImplementations(
   Seq(
     run_return,
-    run_call
+    run_call,
+    run_function
   )
 )
