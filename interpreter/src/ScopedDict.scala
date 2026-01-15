11c40bac0dbde29b7bd121e7603edfb2b61c0ba5
Gabayoyo <148140627+Gabayoyo@users.noreply.github.com>
2026-01-15 13:35:45 +0000

    Refactoring and testing functionality of IR interpreter (#394)
    Co-authored-by: Maks Kret <s2071662@ed.ac.uk>

---
+++ interpreter/src/ScopedDict.scala
@@ -0,0 +1,22 @@
+package scair.interpreter
+
+import scair.ir.*
+
+import scala.collection.mutable
+
+case class ScopedDict(
+    val parent: Option[ScopedDict],
+    scope: mutable.Map[Value[Attribute], Any],
+):
+
+  // recursive get function to find variable in current or parent scopes
+  def get(key: Value[Attribute]): Option[Any] =
+    scope.get(key) match
+      case Some(v) => Some(v)
+      case None    =>
+        parent match
+          case Some(p) => p.get(key)
+          case None    => None
+
+  def update(key: Value[Attribute], value: Any): Unit =
+    scope.update(key, value)
