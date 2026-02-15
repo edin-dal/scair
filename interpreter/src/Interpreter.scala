074d4ad931bbc48a76defa62411d4f74cb4d68ad
Gabayoyo <aarongaba05@gmail.com>
2026-02-15 22:51:47 +0000

    changed interpreter print message

--- interpreter/src/Interpreter.scala
+++ interpreter/src/Interpreter.scala
@@ -153,6 +153,6 @@ class Interpreter(
 
   def interpreter_print(value: Any): Unit =
     value match
-      case 0 => println("false")
-      case 1 => println("true")
+      case 0 => println("Result: false")
+      case 1 => println("Result: true")
       case _ => println(s"Result: $value")
