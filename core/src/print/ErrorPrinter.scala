291761ae61e6868befb0d96691862af4d7dd8b0b
Emilien Bauer <bauer.emilien@gmail.com>
2026-04-15 15:42:48 +0100

    ErrorPrinter signals the Writer to write the message, which will be printed on the next newline instead.

--- core/src/print/ErrorPrinter.scala
+++ core/src/print/ErrorPrinter.scala
@@ -12,15 +12,41 @@ private class ErrorPrinterFilter(writer: Writer) extends PrintWriter(writer):
 
   final val currentColumn = mutable.ListBuffer(0)
 
+  var msg: Option[(String, Int, Int)] = None
+
   @tailrec
-  private final def accountLines(str: String, start: Int): Unit =
+  private final def writeRec(str: String, start: Int): Unit =
     str.indexOf('\n', start) match
       case -1 =>
         currentColumn(currentColumn.length - 1) += str.length - start
+        super.write(str)
       case i =>
         currentColumn(currentColumn.length - 1) += i - start
         currentColumn += 0
-        accountLines(str, i + 1)
+        super.write(str, start, i - start)
+        msg.map(printMessage)
+        writeRec(str, i + 1)
+
+  def nextMessage(content: String, start: Int, end: Int): Unit =
+    // scala.Console.println(s"Received message, current: $lastLength")
+    currentColumn.last match
+      case 0 => // new line, just print the message
+        printMessage(content, start, end)
+      case _ =>
+        msg = Some((content, start, end))
+
+  final def printMessage(content: String, start: Int, end: Int): Unit =
+    msg = None
+    1 to start foreach (_ => super.write(" "))
+    start + 1 to end foreach (_ => super.write("^"))
+    super.write("\n")
+    content.linesIterator.foreach(l =>
+      1 to start foreach (_ => super.write(" "))
+      super.write("> ")
+      super.write(l)
+      super.write("\n")
+    )
+    flush()
 
   def lastLength: Int =
     currentColumn.last match
@@ -30,8 +56,9 @@ private class ErrorPrinterFilter(writer: Writer) extends PrintWriter(writer):
       case l => l
 
   override def write(str: String): Unit =
-    accountLines(str, 0)
-    super.write(str)
+    // scala.Console.println(s"Printing: '$str'")
+    writeRec(str, 0)
+    // scala.Console.println(s"Finished printing, current: $lastLength")
 
 final class ErrorPrinter(
     error: Err,
@@ -71,20 +98,23 @@ final class ErrorPrinter(
       indentLevel,
     )
 
-  final private def withUnderlinedMessage(toPrint: => Unit): Unit =
-    val startColumn = p.asInstanceOf[ErrorPrinterFilter].lastLength
-    toPrint
-    val endColumn = p.asInstanceOf[ErrorPrinterFilter].lastLength
-    1 to startColumn foreach (_ => super.print(" "))
-    startColumn + 1 to endColumn foreach (_ => super.print("^"))
+  final private def printMessage(content: String, start: Int, end: Int): Unit =
+    1 to start foreach (_ => super.print(" "))
+    start + 1 to end foreach (_ => super.print("^"))
     super.print("\n")
     error.msg.linesIterator.foreach(l =>
-      1 to startColumn foreach (_ => super.print(" "))
+      1 to start foreach (_ => super.print(" "))
       super.print("> ")
       super.print(l)
       super.print("\n")
     )
     flush()
+
+  final private def withUnderlinedMessage(toPrint: => Unit): Unit =
+    val startColumn = w.lastLength
+    toPrint
+    val endColumn = w.lastLength
+    w.nextMessage(error.msg, startColumn, endColumn)
 
   override def print(operation: Operation): Unit =
     if obj eq operation then withUnderlinedMessage(super.print(operation))
