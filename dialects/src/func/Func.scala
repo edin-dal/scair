e26770cf8c74a72ecccbd634d7e3604a0c739e19
Emilien Bauer <bauer.emilien@gmail.com>
2026-07-09 12:35:09 +0100

    adjacent tweaks.

--- dialects/src/func/Func.scala
+++ dialects/src/func/Func.scala
@@ -78,7 +78,7 @@ case class Func(
     lprinter.print("func.func ")
     sym_visibility match
       case Some(visibility) =>
-        lprinter.print(visibility.data); lprinter.print(" ")
+        lprinter.print(visibility.data, " ")
       case None => ()
     lprinter.print("@")
     lprinter.print(sym_name.data)
@@ -136,15 +136,15 @@ case class CallIndirect(
   override def verify(): OK[Operation] =
     callee.typ match
       case FunctionType(inTys, outTys) =>
-        if callee_operands.map(_.typ) != inTys.data then
+        if callee_operands.typ != inTys.data then
           Err(
             s"func.call_indirect: argument types ${callee_operands
-                .map(_.typ)} do not match callee input types ${inTys.data}"
+                .typ} do not match callee input types ${inTys.data}"
           )
-        else if _results.map(_.typ) != outTys.data then
+        else if _results.typ != outTys.data then
           Err(
             s"func.call_indirect: result types ${_results
-                .map(_.typ)} do not match callee output types ${outTys.data}",
+                .typ} do not match callee output types ${outTys.data}",
             // TODO: Really not ideal error localization, was done for demonstration purposes, feel free to change it.
             Some(callee),
           )
