203e996497f59d1685ac9f468f84006d44174932
Emilien Bauer <bauer.emilien@gmail.com>
2026-09-02 22:11:21 +0100

    Remove one-call helper.

--- dialects/test/src/LoopLikeTest.scala
+++ dialects/test/src/LoopLikeTest.scala
@@ -20,31 +20,28 @@ class DialectLoopLikeTest extends AnyFlatSpec with BeforeAndAfter:
   def parseOps(text: String): Seq[Operation] =
     Parser(ctx).parse(text).get.value.regions.head.blocks.head.operations.toSeq
 
-  /** Parses `text`, whose last operation is the one under test. */
-  def parseLoop(text: String): Operation = parseOps(text).last
-
   "scf.for" should "be loop like over its body" in {
-    val op = parseLoop("""
+    val op = parseOps("""
 %i = "test.op"() : () -> index
 "scf.for"(%i, %i, %i) ({
 ^bb0(%iv: index):
   "scf.yield"() : () -> ()
 }) : (index, index, index) -> ()
-""")
+""").last
 
     op shouldBe a[LoopLike]
     op.asInstanceOf[LoopLike].loopRegions shouldBe op.regions
   }
 
   "scf.while" should "be loop like over both its regions" in {
-    val op = parseLoop("""
+    val op = parseOps("""
 %c = "test.op"() : () -> i1
 "scf.while"(%c) ({
   "scf.condition"(%c) : (i1) -> ()
 }, {
   "scf.yield"() : () -> ()
 }) : (i1) -> ()
-""")
+""").last
 
     val loop = op.asInstanceOf[LoopLike]
     loop.loopRegions.length shouldBe 2
@@ -52,49 +49,49 @@ class DialectLoopLikeTest extends AnyFlatSpec with BeforeAndAfter:
   }
 
   "scf.parallel" should "be loop like over its body" in {
-    val op = parseLoop("""
+    val op = parseOps("""
 %i = "test.op"() : () -> index
 "scf.parallel"(%i, %i, %i) <{operandSegmentSizes = array<i32: 1, 1, 1, 0>}> ({
 ^bb0(%iv: index):
   "scf.reduce"() : () -> ()
 }) : (index, index, index) -> ()
-""")
+""").last
 
     op.asInstanceOf[LoopLike].loopRegions shouldBe op.regions
   }
 
   "scf.forall" should "be loop like over its body" in {
-    val op = parseLoop("""
+    val op = parseOps("""
 %i = "test.op"() : () -> index
 "scf.forall"(%i, %i, %i) <{staticLowerBound = array<i64: 0>, staticUpperBound = array<i64: 1>, staticStep = array<i64: 1>, operandSegmentSizes = array<i32: 1, 1, 1, 0>}> ({
 ^bb0(%iv: index):
   "scf.forall.in_parallel"() ({
   }) : () -> ()
 }) : (index, index, index) -> ()
-""")
+""").last
 
     op.asInstanceOf[LoopLike].loopRegions shouldBe op.regions
   }
 
   "affine.for" should "be loop like over its body" in {
-    val op = parseLoop("""
+    val op = parseOps("""
 "affine.for"() <{lowerBoundMap = affine_map<() -> (0)>, upperBoundMap = affine_map<() -> (256)>, step = 1 : index, operandSegmentSizes = array<i32: 0, 0, 0>}> ({
 ^bb0(%iv: index):
   "affine.yield"() : () -> ()
 }) : () -> ()
-""")
+""").last
 
     op.asInstanceOf[LoopLike].loopRegions shouldBe op.regions
   }
 
   "affine.parallel" should "be loop like over its body" in {
-    val op = parseLoop("""
+    val op = parseOps("""
 %n = "test.op"() : () -> index
 "affine.parallel"(%n) <{lowerBoundsMap = affine_map<() -> (0)>, lowerBoundsGroups = dense<1> : vector<1xi32>, upperBoundsMap = affine_map<()[s0] -> (s0)>, upperBoundsGroups = dense<1> : vector<1xi32>, steps = [1 : i64], reductions = []}> ({
 ^bb0(%iv: index):
   "affine.yield"() : () -> ()
 }) : (index) -> ()
-""")
+""").last
 
     op.asInstanceOf[LoopLike].loopRegions shouldBe op.regions
   }
